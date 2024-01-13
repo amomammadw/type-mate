d33d0dfb0ac6edb8e5889d1b7f2f5d113787fda9
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-01-13 21:39:24 +0330

    [ADD] jsDoc

--- src/flags.ts
+++ src/flags.ts
@@ -1,7 +1,39 @@
+/**
+ * @description is the value string or not
+ * @returns boolean
+ * @param string your value
+ * @example isString('mmd')
+ */
 export const isString = (value: any) => typeof value === "string";
 
+/**
+ * @description is the value number or not
+ * @returns boolean
+ * @param any your value
+ * @example isNumber(2)
+ */
 export const isNumber = (value: any) => typeof value === "number";
 
+/**
+ * @description is the value undefined or not
+ * @returns boolean
+ * @param any your value
+ * @example isUndefined('mmd')
+ */
 export const isUndefined = (value: any) => typeof value === "undefined";
 
+/**
+ * @description is the value object or not
+ * @returns boolean
+ * @param object your value
+ * @example isObject({name:'mmd'})
+ */
 export const isObject = (value: any) => typeof value === "object";
+
+/**
+ * @description is the value symbol or not
+ * @returns boolean
+ * @param symbol your value
+ * @example isSymbol({name:'mmd'})
+ */
+export const isSymbol = (value: any) => typeof value === "symbol";
--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,9 @@
+/**
+ * @description is the value odd or not
+ * @returns boolean
+ * @param number your value
+ * @example isOdd(31)
+ */
 export const isOdd = (number: number) => {
     if (typeof number === "number") {
         return number % 2 !== 0;
@@ -6,6 +12,12 @@ export const isOdd = (number: number) => {
     }
 };
 
+/**
+ * @description is the value even or not
+ * @returns boolean
+ * @param number your value
+ * @example isEven(31)
+ */
 export const isEven = (number: number) => {
     if (typeof number === "number") {
         return number % 2 === 0;
