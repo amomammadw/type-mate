4e144c3f8fc88e2f0eb8c17d220579f82dfba0af
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-01-18 17:38:31 +0330

    "tada" first version :rocket: new helpers

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export { isEven, isOdd } from "./math";
-export { isNumber, isString } from "./flags";
+export { isEven, isOdd, divide } from "./math";
+export { isNumber, isString, isBoolean, isNull, isObject, isSymbol, isUndefined } from "./flags";
+export { spliceArray } from "./arrays";
+export { toLowerCase } from "./strings";
+export { delay } from "./timing";
---
+++ src/strings.ts
@@ -0,0 +1,6 @@
+/**
+ * @description converts string to lowercase
+ * @param value string
+ * @returns lowercased string
+ */
+export const toLowerCase = (value: string) => value.toLowerCase();
